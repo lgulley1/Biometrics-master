19b3470b0f497cdb893dde74513db85da6d8f343
Luke <lgulley1@kent.edu>
2018-04-19 04:45:02 -0400

    rework of functions

--- face.py
+++ face.py
@@ -1,69 +1,30 @@
 import face_recognition
 import cv2
+from cv2 import *
 import time
+import os
 
-video_capture = cv2.VideoCapture(0)
+def compareTwoImages(img1, img2): #returns similarity score between two jpeg images
+	if img1[len(img1)-4:] != ".jpg" and img1[len(img1)-4:] != ".jpeg":
+		img1 += ".jpg"
+	if img2[len(img2)-4:] != ".jpg" and img2[len(img2)-4:] != ".jpeg":
+		img2 += ".jpg"
+	img1_loaded = face_recognition.load_image_file(img1)
+	img2_loaded = face_recognition.load_image_file(img2)
+	img1_encoded = [face_recognition.face_encodings(img1_loaded)[0]]
+	img2_encoded = face_recognition.face_encodings(img2_loaded)[0]
+	face_distance = face_recognition.face_distance(img1_encoded, img2_encoded)
+	return 1-face_distance[0]
 
-trump1 = face_recognition.load_image_file("trump1.jpg")
-trump2 = face_recognition.load_image_file("trump2.jpg")
-biden = face_recognition.load_image_file("biden.jpg")
-
-trump1_encoding = face_recognition.face_encodings(trump1)[0]
-trump2_encoding = face_recognition.face_encodings(trump2)[0]
-biden_encoding = face_recognition.face_encodings(biden)[0]
-
-# Initialize some variables
-face_locations = []
-face_encodings = []
-face_names = []
-process_this_frame = True
-
-known_face_encodings = [
-	trump1_encoding,
-	trump2_encoding,
-	biden_encoding
-]
-
-known_face_names = [
-	"Trump1",
-	"Trump2",
-	"Biden"
-]
-
-def isRegistered():
-	global process_this_frame
-	# Grab a single frame of video
-	ret, frame = video_capture.read()
-	
-	# Resize frame of video to 1/4 size for faster face recognition processing
-	small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-	
-	# Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-	rgb_small_frame = small_frame[:, :, ::-1]
-	
-	# Only process every other frame of video to save time
-	if process_this_frame:
-		# Find all the faces and face encodings in the current frame of video
-		face_locations = face_recognition.face_locations(rgb_small_frame)
-		face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-	
-		face_names = []
-		time.sleep(3)
-		for face_encoding in face_encodings:
-			# See if the face is a match for the known face(s)
-			matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-	
-			# If a match was found in known_face_encodings, just use the first one.
-			video_capture.release()
-			cv2.destroyAllWindows()
-			if True in matches:
-				return True
-			else:
-				return False
-	
-	process_this_frame = not process_this_frame
-
-if(isRegistered()):
-	print("Accepted")
-else:
-	print("Rejected")
+def captureImage(): #capture faces from webcam and save in format userX.jpg
+	fileExists = True
+	i = 0
+	while(fileExists):
+		if os.path.isfile("user" + str(i) + ".jpg") or os.path.isfile("user" + str(i) + ".jpeg"):
+			i += 1
+		else:
+			fileExists = False
+	cam = cv2.VideoCapture(0)
+	s, img = cam.read()
+	imwrite("user" + str(i) + ".jpg", img)
+	cam.release()
--- mouse_tracker.py
+++ mouse_tracker.py
@@ -1,94 +1,189 @@
+#USAGE
+'''
+Run script.
+To begin recording, left click on very top left of screen. Recording will begin 10 seconds after the click.
+To end recording, right click the very top left of screen.
+Results will be stored in whatever directory the script is run
+*Ensure grid size is correct before running
+'''
+
+import wx
 from pynput import mouse
 from pynput import keyboard
 import timeit
+import time
+import math
 
-leftClicks = 0
-rightClicks = 0
+#forces game to wait until left click on top left of screen
+startGame = False
+
+#the number of quadrants is the square of this number
+gridSize = 100
+
+#stores current position of mouse and time since last quadrant transfer
+pos = -1
+timeSinceLastMove = -1
+
+#for storing result data
 movement = []
 movement_speed = []
-pos = -1
-time = -1
-def updatePos(newPos):
-	global movement_speed
+leftClicks = 0
+rightClicks = 0
+
+#defines screen size
+app = wx.App(False)
+screen_width = wx.GetDisplaySize()[0]
+screen_height = wx.GetDisplaySize()[1]
+
+#records beginning and end of game (measures length of game in time)
+startTime = 0
+endTime = 0
+
+#used to count occurrences
+DISTANCE = 7
+
+#removes all entries in a dictionary which have a second value of 0
+def removeZeroes(d):
+	newD = {}
+	for i in range(1, 10001):
+		if(d[str(i)] != 0):
+			newD.update({str(i) : d[str(i)]})
+	return newD
+
+def counter_cosine_similarity(c1, c2): #find similarity score between two dictionaries
+    terms = set(c1).union(c2)
+    dotprod = sum(c1.get(k, 0) * c2.get(k, 0) for k in terms)
+    magA = math.sqrt(sum(c1.get(k, 0)**2 for k in terms))
+    magB = math.sqrt(sum(c2.get(k, 0)**2 for k in terms))
+    return round((dotprod / (magA * magB)), 2)
+
+
+def countOccurrences(s): #count number of occurences in string (s), based on distance of (DISTANCE), returns dictionary
+	#global DISTANCE
+	r = []
+	for i in range(0, len(s)):
+		if(len(str(s[i:(i+DISTANCE)])) == DISTANCE):
+			r.append(str(s[i:(i+DISTANCE)]))
+	r2 = {}
+	for i in r:
+		r2.update({str(i) : r.count(i)})
+	results = {}
+	r3 = sorted(r2.items(), key=lambda r2: r2[1])
+	for t in r3:
+		results.update({t[0] : t[1]})
+	return results
+
+
+def updatePos(newPos): #updates the new position and saves it to "movement", and saves it's speed from the last quadrant to "movement speed"
 	global pos
-	global time
+	global timeSinceLastMove
 	if pos != newPos:
 		pos = newPos
-		movement.append(pos)
-		if time == -1:
-			time = timeit.default_timer()
-		else:
-			timeChange = timeit.default_timer() - time
-			movement_speed.append(timeChange)
-			time = timeit.default_timer()
+		movement.append(pos) #save position
+		print("Moved to pos: " + str(pos))
+		timeChange = timeit.default_timer() - timeSinceLastMove
+		movement_speed.append(timeChange) #save time
+		timeSinceLastMove = timeit.default_timer()
 
 
-def on_move(x, y):
-	#top 3 boxes
-	if x < 426 and y < 266:
-		updatePos(1)
-	elif x < 852 and y < 266:
-		updatePos(2)
-	elif x < 1280 and y < 266:
-		updatePos(3)
-	#middle 3 boxes
-	elif x < 426 and y < 533:
-		updatePos(4)
-	elif x < 852 and y < 533:
-		updatePos(5)
-	elif x < 1280 and y < 533:
-		updatePos(6)
-	#bottom 3 boxes
-	elif x < 426 and y < 800:
-		updatePos(7)
-	elif x < 852 and y < 800:
-		updatePos(8)
-	elif x < 1280 and y < 800:
-		updatePos(9)
+def on_move(x, y): #detects movement between quadrants
+	if startGame:
+		squareSizeX = screen_width/gridSize #specifies quadrant size
+		squareSizeY = screen_height/gridSize
+		posMovedTo = 0
+		for i in range(1,gridSize+1):
+			for k in range(1,gridSize+1):
+				if x <= squareSizeX*k and y <= squareSizeY*i and x >= (squareSizeX*(k-1)) and y >= (squareSizeY*(i-1)):
+					updatePos(posMovedTo+1)
+				posMovedTo += 1
 
 
 def recordResults():
+	global startTime
+	global endTime
+	overallTime = endTime - startTime
+	average_movement = {}
+	#records every mouse movement as string
 	f = open("mouse_movements.txt", "w+")
-	for i in movement:
-		f.write(str(i))
+	for i in range(0, len(movement)):
+		if i != len(movement) - 1:
+			f.write(str(movement[i]))
+		else:
+			f.write(str(movement[i]))
+	#records average usage of each quadrant
+	#divides each quadrant with a value > 0 by the total number of quadrants with a value > 0
+	movement_count = {}
+	for i in range(1, gridSize**2 + 1):
+		movement_count.update({str(i) : movement.count(i)}) #counts number of times quadrant accessed
+	for i in range(1, gridSize**2 + 1):
+		if(movement_count[str(i)] != 0): #can be modified to accept only higher values, removes all values that = 0 from movement_count
+			average_movement.update({str(i) : movement_count[str(i)]})
+	#average movement is now a dict of strings and their counts, with all 0's removed
+	nAvgMovement = {}
+	for i in average_movement:
+		nAvgMovement.update({str(i) : average_movement[str(i)]/len(average_movement)})
+		
+	f.write("\nLocation Frequency: " + str(nAvgMovement))
 	f.close()
+
+	#records number of clicks
 	f2 = open("clicks.txt", "w+")
-	f2.write("Left Clicks: " + str(leftClicks/2) + "\n")
-	f2.write("Right Clicks: " + str(rightClicks/2))
+	if leftClicks == 0 and rightClicks == 0:
+		f2.write("Left Clicks: 0\n")
+		f2.write("Right Clicks: 0\n")
+	else:
+		f2.write("Left Clicks: " + str((leftClicks)/(leftClicks + rightClicks)) + "\n")
+		f2.write("Right Clicks: " + str((rightClicks)/(leftClicks + rightClicks)) + "\n")
 	f2.close()
+
+	#records mouse speed between quadrants
+	#find value by dividing sum of all movement speeds with the number of movement speeds recorded
 	f3 = open("mouse_speed.txt", "w+")
+	movement_speed.pop(0) #remove first element, because it's the first time and is always incorrect
 	for i in movement_speed:
 		f3.write(str(i) + "\n")
+	f3.write("Average Time: " + str(sum(movement_speed)/len(movement_speed)))
 	f3.close()
 
+	#records all above results 
+	f4 = open("all_results.txt", "w+")
+	finalResults = nAvgMovement
+	if leftClicks == 0 and rightClicks == 0:
+		finalResults.update({"leftClicks" : 0})
+		finalResults.update({"rightClicks" : 0})
+	else:
+		finalResults.update({"leftClicks" : (leftClicks)/(leftClicks + rightClicks)})
+		finalResults.update({"rightClicks" : (rightClicks)/(leftClicks + rightClicks)})
+	finalResults.update({"time" : sum(movement_speed)/len(movement_speed)})
+	f4.write(str(finalResults))
+	f4.close()
 
-def on_click(x, y, button, pressed):
+
+def on_click(x, y, button, pressed): #starts/stops game and records mouse clicks
 	global leftClicks
 	global rightClicks
+	global startGame
+	global startTime
+	global endTime
 	if button == mouse.Button.left:
 		leftClicks += 1
 	if button == mouse.Button.right:
 		rightClicks += 1
-	if x == 0 and y == 0 and button == mouse.Button.left:
+	if x == 0 and y == 0 and button == mouse.Button.left: #starts recording
 		leftClicks -= 1
-		recordResults()
+		startTime = timeit.default_timer()
+		startGame = True
+	if x == 0 and y == 0 and button == mouse.Button.right: #ends recording
+		rightClicks -= 1
+		endTime = timeit.default_timer()
 		return False
 
 
-def on_press(key):
-    try:
-        print('alphanumeric key {0} pressed'.format(
-            key.char))
-    except AttributeError:
-        print('special key {0} pressed'.format(
-            key))
-
-
-key_listener = keyboard.Listener(on_release=on_press)
-
-
-# Collect events until released
+#collects mouse events until released
 with mouse.Listener(
 		on_move=on_move,
 		on_click=on_click) as listener:
 	listener.join()
+
+#save results
+recordResults()
